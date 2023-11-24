99ba51d5e080c0063bf745a4b58a8fd269f7d358
Alden Shin-Culhane <aldenshinculhane@gmail.com>
2023-11-24 15:25:06 -0500

    Current state

--- src/main/java/DatabaseOperations.java
+++ src/main/java/DatabaseOperations.java
@@ -3,6 +3,7 @@ package src.main.java;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import javax.swing.*;
 
 public class DatabaseOperations {
 
@@ -40,6 +41,16 @@ public class DatabaseOperations {
     }
 
     public static void queryTables() {
-        executeShellScript("shell_scripts/query_tables.sh");
+        String[] options = {"Query 1", "Query 2", "Query 3", "Query 4", "Query 5", "Query 6"};
+        int choice = JOptionPane.showOptionDialog(null,
+                "Select a query to execute:",
+                "Choose Query",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null, options, options[0]);
+
+        if (choice != -1) {
+            executeShellScript("shell_scripts/query_tables.sh " + (choice + 1));
+        }
     }
 }
